f7e8f952ffc4c4a8490abcc7a7a114fb0081e5d1
serenitybomb <51345088+serenitybomb@users.noreply.github.com>
2023-07-12 14:38:58 -0500

    aesthetic & timestamp fixes

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,24 @@ let router = express.Router();
 import { getAllPosts } from "../requests.js";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime.js";
+import utc from "dayjs/plugin/utc.js";
 
+// https://day.js.org/docs/en/plugin/utc
+// https://day.js.org/docs/en/plugin/relative-time
 dayjs.extend(relativeTime);
+dayjs.extend(utc);
 
 router.get("/", async function (req, res, next) {
   const posts = await getAllPosts();
   for (const post of posts) {
-    post.time = dayjs(post.time).fromNow();
+    // //timestamp is stored as utc in db; need to convert to local time
+    // post.time = dayjs.utc(post.time).local().format();
+    // //then as relative time
+    // post.time = dayjs(post.time).fromNow();
+    // as one line:
+    post.time = dayjs(dayjs.utc(post.time).local().format()).fromNow();
   }
-  console.log(posts);
+  // console.log(posts);
   res.render("index", {
     title: "Classifieds",
     user: req.user,
--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,19 @@
 import express from "express";
 let router = express.Router();
-import axios from "axios";
 import { getUserPosts } from "../requests.js";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime.js";
+import utc from "dayjs/plugin/utc.js";
+
+dayjs.extend(relativeTime);
+dayjs.extend(utc);
 
 router.get("/", async function (req, res, next) {
   if (req.user) {
     const posts = await getUserPosts(req.user.username);
+    for (const post of posts) {
+      post.time = dayjs(dayjs.utc(post.time).local().format()).fromNow();
+    }
     // console.log(posts);
     res.render("profile", {
       title: "Classifieds - Profile",
